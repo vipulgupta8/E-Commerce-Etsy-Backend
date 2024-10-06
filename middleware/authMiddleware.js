480b3fbba501dfb9e7ec62e5f40e55e57aafea5a
Vipul Gupta <147442104+vipulgupta8@users.noreply.github.com>
2024-10-06 22:35:17 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,29 +1,8 @@
 
-// import jwt from 'jsonwebtoken';
-// import { errorHandler } from '../utils/error.js';
-
-// export const verifyToken = (req, res, next) => {
-//   const token = req.cookies.access_token;
-
-//   if (!token) {
-//     return next(errorHandler(401, 'Access denied, no token provided'));
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.userId = decoded.id; // Attach the user's ID to the request
-//     next();
-//   } catch (error) {
-//     return next(errorHandler(403, 'Invalid or expired token'));
-//   }
-// };
-
 import jwt from 'jsonwebtoken';
 import { errorHandler } from '../utils/error.js';
 
 export const verifyToken = (req, res, next) => {
-  // Log cookies for debugging
-  console.log('Cookies:', req.cookies);
 
   // Get the token from cookies
   const token = req.cookies.access_token; // Token expected in cookies
